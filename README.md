91be412d5d6e567bfb9bcc17574dc471c04e4904
Sebiann <redstonetriforce@gmail.com>
2019-06-19 20:30:34 +0200

    Started the work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Sebiann.moe
+
+My Super dupper website written using PUG, YES A FRICKING PUG
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Sebiann.moe' });
 });
 
 module.exports = router;
